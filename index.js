007343023ca5ef56158f6e78e45dc9a5579279d3
stella-celentano <stellacelentano99@gmail.com>
2020-08-19 10:06:58 -0300

    Setting entrypoint file

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+const database = require('./src/config/database')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
+app.use(bodyParser.json({ type: 'application/json' }))
+
+app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow_Header', 'Origin, X_requested-With, Content-Type, Accept')
+    next() 
+})
+
+app.use('/', (req, res) => res.send({ message: `API listening in port ${PORT}` }) )
+
+app.use('*', (req, res) => res.send({ message: 'API not found' }))
+
+app.listen(PORT, () => {
+    console.log(`API listening in port ${PORT}`)
+})
